681bc0e5ca5828a0dae8429455b1d58618d6966d
aestrivex <aestrivex@gmail.com>
2015-02-03 16:59:43 -0500

    some changes to constants and montage

--- mne/channels/layout.py
+++ mne/channels/layout.py
@@ -898,8 +898,8 @@ def read_montage(kind, ch_names=None, path=None, scale=True):
     return Montage(pos=pos, ch_names=ch_names_, kind=kind, selection=selection)
 
 
-def apply_montage(info, montage):
-    """Apply montage to EEG data.
+def apply_montage(info, montage, replace_as_seeg=False):
+    """Apply montage to EEG or sEEG data.
 
     This function will replace the EEG channel names and locations with
     the values specified for the particular montage.
@@ -912,8 +912,11 @@ def apply_montage(info, montage):
         The measurement info to update.
     montage : instance of Montage
         The montage to apply.
+    replace_as_seeg : bool
+        If True, sets the channel type of the channels in the montage to sEEG
+        and sets the coordinate type as surface coordinates.
     """
-    if not _contains_ch_type(info, 'eeg'):
+    if not _contains_ch_type(info, 'eeg') and not replace_as_seeg:
         raise ValueError('No EEG channels found.')
 
     sensors_found = False
@@ -926,6 +929,10 @@ def apply_montage(info, montage):
         info['chs'][ch_idx]['eeg_loc'] = np.c_[pos, [0.] * 3]
         info['chs'][ch_idx]['loc'] = np.r_[pos, [0.] * 9]
         sensors_found = True
+
+        if replace_as_seeg:
+            info['chs'][ch_idx]['kind'] = FIFF.FIFF_SEEG_CH
+            info['chs'][ch_idx]['coord_frame'] = FIFF.COORD_MRI
 
     if not sensors_found:
         raise ValueError('None of the sensors defined in the montage were '
